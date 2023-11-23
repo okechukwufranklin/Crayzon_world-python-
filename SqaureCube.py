40a4912f7a43f2e89bc6394b31a6092090209737
okechukwufranklin <anoluefrank69@gmail.com>
2023-11-23 09:03:44 +0100

    first commit

---
+++ SqaureCube.py
@@ -0,0 +1,7 @@
+print('number\t square\t cube')
+print('0\t',(0*0)\t (0*0*0))
+print('1\t (1*1)\t (1*1*1)')
+print('2\t (2*2)\t (2*2*2)')
+print('3\t (3*3)\t (3*3*3)')
+print('4\t (4*4)\t (4*4*4)')
+print('5\t (5*5)\t (5*5*5)')

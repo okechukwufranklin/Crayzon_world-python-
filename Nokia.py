33eccbd94c8c09313dc4c71b9e7867d7149a9f04
okechukwufranklin <anoluefrank69@gmail.com>
2023-12-04 15:40:14 +0100

    nokia

---
+++ Nokia.py
@@ -0,0 +1,85 @@
+
+	
+
+print("1.Phonebook\n2.Messages\n3.Chat\n4.Call Register\n5.Tones\n6.Settings\n7.Call Divert\n8.Games")
+print("\n9.Calculator\n10.Reminder\n11.Clock\n12.Profiles\n13.Sim Services\n")
+number =int(input("Enter number:"))
+
+if(number == 1):
+ print("Press\n")
+ print("\n1.Search\n2.Service Nos\n3.Add Name\n4.Erase\n5.Edit\n6.Assign Tone\n7.Send b' Card")
+ print("\n8.Options\n9.Spead dails\n10.Voice tags\n")
+numb = int(input("Enter Number:"))
+
+if(numb == 2):
+	print("Servis Nos")
+
+elseif(numb == 3)
+print("Add Number") 
+
+elseif(numb == 4)
+print("Erase")
+
+elseif(numb == 5)
+print("Edit")
+
+elseif(numb == 6)
+print("Assign Tone")
+
+elseif(numb == 7)
+print("Send b'card")
+
+elseif(numb == 9)
+print("Spead dial")
+
+elseif(numb == 10)
+print("Voice Tag")
+
+elseif(numb == 8)
+print("Press")
+print("\n1.Type of View\n2.Memory Status\n")
+numbers = int(input("Enter Number:"))
+
+if(numbers == 1):
+	print("Type of View");
+
+if(numbers == 2):
+	print("Memory Full");
+
+
+if(number == 2):
+		print("Press\n")
+		print("\n1.Write Message\n2.Inbox\n3.Outbox\n4.Picture Messages\n5.Templates\n6.Smileys\n7.Message settings\n8.Info service\n9.Voice mailbox number\n10.services command editor\n")
+		numb = int(input("Enter Number:"))
+
+
+if(numb == 1):
+	print("Write Message")
+
+elseif(numb == 2)
+print("Ibox")
+
+elseif(numb == 3)
+print("Outbox") 
+
+elseif(numb == 4)
+print("Picture message")
+
+elseif(numb == 5)
+print("Templates")
+
+elseif(numb == 6)
+print("smileys")
+
+elseif(numb == 8)
+print("Info Service")
+
+elseif(numb == 9)
+print("Voice mailbox number")
+
+elseif(numb == 10)
+print("Service command editor")
+
+elseif(numb == 7)
+print("Set 1\n2.Common\n")
+

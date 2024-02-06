80a953b925b4d5a4e4cb0d7293207b7316298f86
okechukwufranklin <151369058+okechukwufranklin@users.noreply.github.com>
2024-02-06 01:42:14 +0100

    update 1.0 for phoneBook

--- PhoneBook.py
+++ PhoneBook.py
@@ -1,29 +1,81 @@
 PhoneBook = []
 
-def PhoneBook_Display():
-    PhoneBook_Menu = """ PhoneBook Menu
-1.Save Number
-2.Edit Number
-3.Delete Number
-4.Search Number
-5.View Contacts
-6.Exit"""
-
-
 def Save_Number():
     contact_info = {}
-    number = int(input("How Many Numbers would u like to save:"))
-    for i in range (number):
-     ContactName = input("Enter Name:")
+    ContactName = input("Enter Name:")
     ContactNumber = int(input("Enter Phone Number:"))
     contact_info[ContactName] = ContactNumber
     PhoneBook.append(contact_info)
-    print("Saved contact:")
-    print(len(contact_info))
-    print(contact_info)
+    PhoneBook_Display()
 
 def Delete_Number(name):
-    input("Enter Name To Delete:")
+    for contact_info in PhoneBook:
+        if name in contact_info:
+            del contact_info[name]
+            print(f"Contact for '{name}' deleted successfully.")
+            PhoneBook_Display()
+            return
+    print(f"Contact for '{name}' not found in the PhoneBook.")
+
+    print("1.back\n 2.Exit")
+    number = int(input())
+    if number == 1:
+        PhoneBook_Display()
+    if number == 2:
+        pass
 
 
-    
+def Search_Name(name):
+    for contact_info in range(len(PhoneBook)):
+        if name in PhoneBook[contact_info]:
+            print(PhoneBook[contact_info])
+    print("1.back\n 2.Exit")
+    number = int(input())
+    if number == 1:
+        PhoneBook_Display()
+    if number == 2:
+        pass
+
+
+def Veiw_Contacts():
+    for contact_info in PhoneBook:
+        print(PhoneBook)
+        return PhoneBook
+    print("1.back\n 2.Exit")
+
+    number = int(input())
+    if number == 1:
+         PhoneBook_Display()
+    if number == 2:
+        pass
+
+
+def Edit_Contacts():
+    print("feature coming Soon")
+    print("Stay Tune For Next Update")
+    print("1.back\n 2.Exit")
+    number = int(input())
+    if number == 1:
+        PhoneBook_Display()
+    if number == 2:
+        pass
+
+
+
+def PhoneBook_Display():
+    number = int(input("1.Save Number\n2.Edit Number\n3.Delete Number\n4.Search Number\n5.View Contacts\n"))
+    if number == 1:
+        Save_Number()
+    if number == 2:
+        Edit_Contacts()
+    if number == 3:
+        name = input("Enter Name To Delete")
+        Delete_Number(name)
+    if number== 4:
+        name = input("Enter Name To Search")
+        Search_Name(name)
+    if number == 5:
+        Veiw_Contacts()
+
+
+PhoneBook_Display()

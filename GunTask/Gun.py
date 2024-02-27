a8a67f3f1601787cb882a5bfcf30bbebe742e3c8
okechukwufranklin <151369058+okechukwufranklin@users.noreply.github.com>
2024-02-27 09:07:28 +0100

    Needs some updates but iTS DONE

--- GunTask/Gun.py
+++ GunTask/Gun.py
@@ -1,17 +1,22 @@
 class Gun:
 
-    def __init__(self, Bullet=0):
+    def __init__(self, Bullet,pin):
         self.Bullet = Bullet
+        self.pin = pin
 
 
 
     def is_empty(self):
         return self.Bullet == 0
 
-    def Shoot_Gun(self,):
+    def Shoot_Gun(self,pin):
+        if self.pin != pin:
+            raise ValueError
         self.Bullet -= 1
 
     def reload_Gun(self):
+        if self.Bullet > 5:
+            return "Magazine full"
         self.Bullet += 5
 
     def Checkammo(self):
